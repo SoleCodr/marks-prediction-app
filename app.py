cd58216630e3e8c759109264b68a1370028afe34
Kuldeep Sharma <sharma.kuldeep249@gmail.com>
2020-08-23 14:42:06 +0530

    updated

--- app.py
+++ app.py
@@ -1,32 +1,44 @@
 import streamlit as st
 import numpy as np
-import pickle
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
-Linear_Reg = pickle.load(open("Linear_Reg.pkl","rb"))
+st.write("""
+# Simple Marks Prediction App
+This app predicts the **Marks** scored by the student!
+""")
 
-def pred(hours):
-    val=np.array([[hours]]).astype(np.float64)
-    predection=Linear_Reg.predict(val)
-    return float(predection)
-
-def main():
-    st.title(" Score Prediction")
-    st.sidebar.markdown('''
-            By Kuldeep Sharma aka [SoleCodr](https://github.com/SoleCodr)
+st.sidebar.markdown('''
+            By Kuldeep Sharma aka [SoleCodr](https://github.com/SoleCodr) \n
+            GitHub Repo for the [App](https://github.com/SoleCodr/marks-prediction-app)
     ''')
-    st.write('''
+st.write('''
         **Assuming the study hours to be maximum 10 hours.**
         ''')
     
-    hours=st.number_input("Enter Number of Hours Of Study",0.00,10.00,4.88)
-    if float(hours)>9.88:
-        if st.button("Predict"):
-            st.success("The Predicted Percentage is 100.00 ")
-    else:
-        if st.button("Predict"):
-            output=round(pred(hours),2)
-            st.success("The Predicted Percentage is {}".format(output))
 
+def user_input():
+    hr = st.number_input("Number of Hours of Study",0.00,10.00,6.2)
+    hr = np.array([[hr]]).astype(np.float64)
+    return hr
 
-if  __name__=="__main__":
-    main()
+pred = user_input()
+
+#Loading dataset
+df = pd.read_csv("https://raw.githubusercontent.com/SoleCodr/Prediction-of-Marks-scored-by-student/master/Data.txt?token=AGEY7MBIGAHSIIG6U7BN7ES7FWEOO")
+
+attr = df.iloc[:,:-1].values
+labels = df.iloc[:,1].values
+
+LR = LinearRegression()
+LR.fit(attr,labels)
+
+prediction = round(float(LR.predict(pred)),2)
+
+if pred > 9.97:
+    st.button("Predict")
+    st.success("The Predicted Percentage is 100.00")
+else:
+    st.button("Predict")
+    st.success("The Predicted Percentage is {}".format(prediction))
+ 
